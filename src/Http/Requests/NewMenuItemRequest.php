b8bb49dc23250b852ba0767bb04e8f79093c22c7
Azer Mammadov <energon7777@gmail.com>
2020-04-23 01:57:28 +0400

    fix

--- src/Http/Requests/NewMenuItemRequest.php
+++ src/Http/Requests/NewMenuItemRequest.php
@@ -25,14 +25,23 @@ class NewMenuItemRequest extends FormRequest
      */
     public function rules()
     {
+       \Validator::extend('uniqueTwoColumns', function ($attribute, $value, $parameters, $validator)  {
+
+            $count = \DB::table('menu_items')
+                ->where('menu_id', request()->get('menu_id'))
+                ->where('id','<>',$parameters[1])
+                ->where('page_id', $value)
+                ->count();
+            return $count === 0;
+        });
         $rules = [
             'menu_id' => 'required|exists:menus,id',
-            'name'    => 'required',
-			'page_id' => 'nullable|unique:menu_items,page_id',
+            'name'    => 'required', //
+            'page_id' => 'nullable|uniqueTwoColumns:'.request()->get('page_id').','.request()->get('id'),
             'type'    => 'required|in:link,route',
             'target'  => 'required|in:_self,_blank',
         ];
-
+		
         if (request()->get('type') == 'link') {
             $rules['url'] = 'required';
         }
